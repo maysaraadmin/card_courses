ba42c033a66aeeb2e4986293b38f7ee63b5a4878
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-04-17 14:27:22 +0400

    fix

--- lang/en/block_card_courses.php
+++ lang/en/block_card_courses.php
@@ -1,36 +1,40 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_card_courses', language 'en'.
+ *
+ * @package   block_card_courses
+ * @copyright 2023 Your Name <your@email.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $string['pluginname'] = 'Card Courses';
-$string['card_courses'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Courses';
+$string['defaulttitle'] = 'Featured Categories';
 $string['blocktitle'] = 'Block Title';
 $string['rootcategory'] = 'Root Category';
 $string['rootcategorydesc'] = 'Select the root category to display courses from';
 $string['showcategories'] = 'Show Categories';
 $string['showcategoriesdesc'] = 'Display category cards';
-$string['showcourses'] = 'Show Courses';
-$string['showcoursesdesc'] = 'Display course cards';
+$string['showsubcategories'] = 'Show Subcategories';
+$string['showsubcategoriesdesc'] = 'Display subcategories under each category';
 $string['maxcategories'] = 'Maximum Categories';
 $string['maxcategoriesdesc'] = 'Maximum number of categories to display';
-$string['maxcourses'] = 'Maximum Courses';
-$string['maxcoursesdesc'] = 'Maximum number of courses to display';
-$string['card_courses:addinstance'] = 'Add a new Card Courses block';
-$string['card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['card_courses:addinstance'] = 'Add a new Card Courses block';
-$string['card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['card_courses:manage'] = 'Manage Card Courses block settings';
-$string['card_courses:view'] = 'View Card Courses block';
-$string['card_courses:edit'] = 'Edit Card Courses block content';
-$string['allcategories'] = 'All Categories';
-$string['backto'] = 'Back to {$a}';
-$string['backtoallcategories'] = 'Back to all categories';
-$string['subcategories'] = 'Subcategories';
-$string['coursesin'] = 'Courses in {$a}';
-$string['viewcoursesin'] = 'View courses in {$a}';
-$string['breadcrumb'] = 'Navigation';
-$string['pluginname'] = 'Card Courses';
-$string['card_courses'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Courses';
+$string['maxsubcategories'] = 'Maximum Subcategories';
+$string['maxsubcategoriesdesc'] = 'Maximum number of subcategories to display per category';
 $string['allcategories'] = 'All Categories';
 $string['backto'] = 'Back to {$a}';
 $string['backtoallcategories'] = 'Back to all categories';
@@ -39,43 +43,13 @@ $string['coursesin'] = 'Courses in {$a}';
 $string['viewcoursesin'] = 'View courses in {$a}';
 $string['breadcrumb'] = 'Navigation';
 $string['courses'] = 'Courses';
-$string['pluginname'] = 'Card Courses';
-$string['card_courses'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Courses';
-$string['courses'] = 'courses';
 $string['viewcourse'] = 'View course {$a}';
 $string['viewcategory'] = 'View category {$a}';
-$string['block_card_courses:addinstance'] = 'Add a new Card Courses block';
-$string['block_card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['nocontent'] = 'No courses or categories available to display';
-$string['pluginname'] = 'Card Courses';
-$string['card_courses'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Courses';
-$string['courses'] = 'courses';
-$string['viewcourse'] = 'View course {$a}';
-$string['viewcategory'] = 'View category {$a}';
-$string['nocontent'] = 'No courses or categories available to display';
-$string['block_card_courses:addinstance'] = 'Add a new Card Courses block';
-$string['block_card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['pluginname'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Categories';
-$string['courses'] = 'courses';
-$string['viewcategory'] = 'View category {$a}';
 $string['nocontent'] = 'No categories available to display';
+$string['nocourses'] = 'No courses available to display';
+
+// Capabilities
 $string['block_card_courses:addinstance'] = 'Add a new Card Courses block';
 $string['block_card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['pluginname'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Categories';
-$string['courses'] = 'courses';
-$string['viewcategory'] = 'View category {$a}';
-$string['nocontent'] = 'No categories available to display';
-$string['block_card_courses:addinstance'] = 'Add a new Card Courses block';
-$string['block_card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['pluginname'] = 'Card Courses';
-$string['defaulttitle'] = 'Featured Categories';
-$string['courses'] = 'courses';
-$string['viewcategory'] = 'View category {$a}';
-$string['nocontent'] = 'No categories available to display.';
-$string['block_card_courses:addinstance'] = 'Add a new Card Courses block';
-$string['block_card_courses:myaddinstance'] = 'Add a new Card Courses block to Dashboard';
-$string['subcategories'] = 'Subcategories';
+$string['block_card_courses:manage'] = 'Manage Card Courses block settings';
+$string['block_card_courses:view'] = 'View Card Courses block';
