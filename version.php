3531629265f64dc7f17717257b05efea962382ba
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-04-17 23:51:26 +0400

    fix

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024020101;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2024020104;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020061500;        // Requires this Moodle version (3.9)
 $plugin->component = 'block_card_courses'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
